9b6824ac004910fe8e78fd8e6b107245221d12b6
Douglas <jdps@cin.ufpe.br>
2023-03-10 00:06:00 -0300

    creating a very minimal POST request

--- monitoring/app/main.py
+++ monitoring/app/main.py
@@ -3,15 +3,18 @@ import uvicorn
 from fastapi import FastAPI
 from api.routers import router
 
+from Registro import Registro
 
 app = FastAPI(title='Monitoramento de modelos', version="1.0.0")
-
 
 @app.get("/")
 def read_root():
     """Hello World message."""
     return {"Hello World": "from FastAPI"}
 
+@app.post("/")
+def post_data(registro: Registro):
+    return {"name":registro.VAR2,"idade":registro.IDADE,"TARGET":registro.TARGET}
 
 app.include_router(router, prefix="/v1")
 
